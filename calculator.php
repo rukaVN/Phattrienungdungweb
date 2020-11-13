b4cc4d8a49da7c25e70f44eaa69fde5adcf63398
rukaVN <63278717+rukaVN@users.noreply.github.com>
2020-11-13 07:51:02 +0700

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -40,56 +40,51 @@
 
 
 <body>
-	<?php
-	$number1 = '';
-	$calculate = '';
-	$number2 = '';
-	$result='';
-	$flag=false;
+<?php
+  $number1 = '';
+  $calculate = '';
+  $number2 = '';
+  $result = '';
+  $flag = false;
 
-	//neu Submit khong la nut Xoa
-	if (isset($_POST['typeSubmit'])&& $_POST['typeSubmit']!='Xoa')
-	{
-		//neu cac o nhap co du lieu
-			if(isset($_POST['number1'])&& isset($_POST['calculate'])&& isset($_POST['number2']))
-			{
-				$number1=$_POST['number1'];
-				$calculate=$_POST['calculate'];
-				$number2=$_POST['number2'];
-				//hai o number1 va number2 la hai so
-				if(is_numeric($number1)&& is_numeric($number2))
-				{
-					$flag=true;
-					//phep toan hop le
-					switch ($calculate)
-					{
-						case '+':
-							$result = $number1 + $number2;
-							break;
-						
-						case '-'
-							$result = $number1 - $number2;
-							break;
-						case '*'
-							$result = $number1 * $number2;
-							break;
-						case '/'
-							$result = $number1 / $number2;
-							break;
-						default:
-						$result = 'Nhap du lieu khong hop le';
-						$flag=false;
-						break;
-					}
-				}else {
-					$result = 'Nhap du lieu khong hop le';
-					$flag=false;
-				}
-			}else{
-				$flag=false;
-			}
-	}
-	?>
+  // nếu submit không là nút Xóa
+  if (isset($_POST['typeSubmit']) && $_POST['typeSubmit'] != 'Xóa') {
+    // nếu các ô nhập có dữ liệu
+    if (isset($_POST['number1']) && isset($_POST['calculate']) && isset($_POST['number2'])) {
+      $number1 = $_POST['number1'];
+      $calculate = $_POST['calculate'];
+      $number2 = $_POST['number2'];
+    // hai ô Số thứ nhất và Số thư hai là hai số
+      if (is_numeric($number1) && is_numeric($number2)) {
+        $flag = true;
+      // phép toán hợp lệ
+        switch ($calculate) {
+          case '+':
+          $result = $number1 + $number2;
+          break;
+          case '-':
+          $result = $number1 - $number2;
+          break;
+          case '*':
+          $result = $number1 * $number2;
+          break;
+          case '/':
+          $result = $number1 / $number2;
+          break;
+          default:
+          $result = 'Nhập dữ liệu không hợp lệ!';
+          $flag = false;
+          break;
+        }
+      } else {
+        $result = 'Nhập dữ liệu không hợp lệ!';
+        $flag = false;
+      }
+    } else {
+      $flag = false;
+    } 
+  } 
+?>
 	<div class="content">
 		<h1>Mo phong may tinh dien tu</h1>
 		<form action ="#" method="post">
